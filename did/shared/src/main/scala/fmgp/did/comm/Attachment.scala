e0c49a970016829149270f5e25bf060173d071af
FabioPinheiro <fabiomgpinheiro@gmail.com>
2022-12-20 22:19:25 +0000

    Fix AttachmentData Data

--- did/shared/src/main/scala/fmgp/did/comm/Attachment.scala
+++ did/shared/src/main/scala/fmgp/did/comm/Attachment.scala
@@ -41,14 +41,44 @@ sealed trait AttachmentData
 case class AttachmentDataJWS(jws: JWS_WithOutPayload) extends AttachmentData
 case class AttachmentDataLinks(links: String, hash: Required[String]) extends AttachmentData
 case class AttachmentDataBase64(base64: Base64 /*, hash: NotRequired[String]*/ ) extends AttachmentData
-case class AttachmentDataJson(json: Seq[Json] /*, hash: NotRequired[String]*/ ) extends AttachmentData
+case class AttachmentDataJson(json: Json /*, hash: NotRequired[String]*/ ) extends AttachmentData
+
+/** A JSON object that gives access to the actual content of the attachment.
+  *
+  * This MUST contain at least one of the following subfields, and enough of them to allow access to the data:
+  *
+  * @param jws
+  *   OPTIONAL. A JWS in detached content mode, where the payload field of the JWS maps to base64 or to something
+  *   fetchable via links. This allows attachments to be signed. The signature need not come from the author of the
+  *   message.
+  * @param hash
+  *   OPTIONAL. The hash of the content encoded in multi-hash format. Used as an integrity check for the attachment, and
+  *   MUST be used if the data is referenced via the links data attribute.
+  * @param links
+  *   OPTIONAL. A list of zero or more locations at which the content may be fetched. This allows content to be attached
+  *   by reference instead of by value.
+  * @param base64
+  *   OPTIONAL. Base64url-encoded data, when representing arbitrary content inline instead of via links.
+  * @param json
+  *   OPTIONAL. Directly embedded JSON data, when representing content inline instead of via links, and when the content
+  *   is natively conveyable as JSON.
+  */
+case class AttachmentDataAny(
+    jws: Option[JWS_WithOutPayload],
+    hash: Option[String],
+    links: Option[String],
+    base64: Option[Base64],
+    json: Option[Seq[Json]],
+) //extends AttachmentData
 
 object AttachmentData {
   given decoder: JsonDecoder[AttachmentData] =
-    AttachmentDataJWS.decoder.widen[AttachmentData] <>
-      AttachmentDataLinks.decoder.widen[AttachmentData] <>
+    AttachmentDataLinks.decoder.widen[AttachmentData] <>
       AttachmentDataBase64.decoder.widen[AttachmentData] <>
-      AttachmentDataJson.decoder.widen[AttachmentData]
+      AttachmentDataJson.decoder.widen[AttachmentData] <>
+      AttachmentDataJWS.decoder
+        .mapOrFail { e => if (e.jws == Json.Null) Left("jws can not be null") else Right(e) } // TODO JWS_WithOutPayload
+        .widen[AttachmentData]
 
   given encoder: JsonEncoder[AttachmentData] = new JsonEncoder[AttachmentData] {
     override def unsafeEncode(b: AttachmentData, indent: Option[Int], out: zio.json.internal.Write): Unit = b match
@@ -73,35 +103,4 @@ object AttachmentDataBase64 {
 object AttachmentDataJson {
   given decoder: JsonDecoder[AttachmentDataJson] = DeriveJsonDecoder.gen[AttachmentDataJson]
   given encoder: JsonEncoder[AttachmentDataJson] = DeriveJsonEncoder.gen[AttachmentDataJson]
-}
-
-/** A JSON object that gives access to the actual content of the attachment.
-  *
-  * This MUST contain at least one of the following subfields, and enough of them to allow access to the data:
-  */
-trait AttachmentDataAtLeastOne {
-
-  /** OPTIONAL. A JWS in detached content mode, where the payload field of the JWS maps to base64 or to something
-    * fetchable via links. This allows attachments to be signed. The signature need not come from the author of the
-    * message.
-    */
-  def jws: Option[JWS_WithOutPayload]
-
-  /** OPTIONAL. The hash of the content encoded in multi-hash format. Used as an integrity check for the attachment, and
-    * MUST be used if the data is referenced via the links data attribute.
-    */
-  def hash: Option[String]
-
-  /** OPTIONAL. A list of zero or more locations at which the content may be fetched. This allows content to be attached
-    * by reference instead of by value.
-    */
-  def links: Option[String]
-
-  /** OPTIONAL. Base64url-encoded data, when representing arbitrary content inline instead of via links. */
-  def base64: Option[Base64]
-
-  /** OPTIONAL. Directly embedded JSON data, when representing content inline instead of via links, and when the content
-    * is natively conveyable as JSON.
-    */
-  def json: Option[Seq[Json]]
 }
--- did/shared/src/main/scala/fmgp/did/comm/package.scala
+++ did/shared/src/main/scala/fmgp/did/comm/package.scala
@@ -1,9 +1,11 @@
 package fmgp.did
 
+import zio.json.ast.Json
+
 package object comm {
 
   type UTCEpoch = Long
-  type JSON_RFC7159 = Map[String, String] // FIXME
+  type JSON_RFC7159 = Json // FIXME
   type Payload = String // Base64URL // TODO REMOVE
 
 }
