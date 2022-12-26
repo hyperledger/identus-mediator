d80fdea1d3953bfb0b34dfe7c8890ddb943d8b6b
FabioPinheiro <fabiomgpinheiro@gmail.com>
2022-12-26 21:59:03 +0000

    Add encoder and decoder to CryptoFailed

--- did/shared/src/main/scala/fmgp/crypto/CryptoFailed.scala
+++ did/shared/src/main/scala/fmgp/crypto/CryptoFailed.scala
@@ -2,9 +2,19 @@ package fmgp.crypto //FIXME
 
 // sealed abstract class
 // object CryptoFail {
+import zio.json._
+
 package error {
 
+  @jsonDiscriminator("type")
   sealed trait DidFail // extends Exception with Product with Serializable
+  object DidFail {
+    given decoderDidFail: JsonDecoder[DidFail] = DeriveJsonDecoder.gen[DidFail]
+    given encodeDidFail: JsonEncoder[DidFail] = DeriveJsonEncoder.gen[DidFail]
+    given decoderCryptoFailed: JsonDecoder[CryptoFailed] = DeriveJsonDecoder.gen[CryptoFailed]
+    given encodeCryptoFailed: JsonEncoder[CryptoFailed] = DeriveJsonEncoder.gen[CryptoFailed]
+  }
+
   case class DidException(error: FailToParse) extends Exception(error.error) // with DidFail
 
   case class FailToParse(error: String) extends DidFail
@@ -15,12 +25,14 @@ package error {
   // ### Error Crypto ###
   // ####################
   sealed trait CryptoFailed extends DidFail
+
   case class FailToGenerateKey(origin: DidFail) extends CryptoFailed
 
   case object CryptoNotImplementedError extends CryptoFailed
   case object UnknownError extends CryptoFailed
-  case class SomeThrowable(throwable: Throwable) extends CryptoFailed {
-    override def toString(): String = throwable.getClass.getName() + ":" + throwable.getMessage
+  case class SomeThrowable(error: String) extends CryptoFailed
+  object SomeThrowable {
+    def apply(throwable: Throwable) = new SomeThrowable(throwable.getClass.getName() + ":" + throwable.getMessage)
   }
   case class CryptoErrorCollection[E <: CryptoFailed](errors: Seq[E]) extends CryptoFailed
   object CryptoErrorCollection {
@@ -57,8 +69,8 @@ package error {
   case object ZeroResults extends CryptoFailed
 
   // Warn Crypto
-  sealed trait CryptoWarn extends Product with Serializable // Exception with
+  sealed trait CryptoWarn // extends Product with Serializable // Exception with
   case class MissDecryptionKey(kid: String) extends CryptoWarn
-  case class UncatchWarning[E <: CryptoWarn](warn: E) extends CryptoFailed
+  // case class UncatchWarning[E <: CryptoWarn](warn: E) extends CryptoFailed
 
 }
