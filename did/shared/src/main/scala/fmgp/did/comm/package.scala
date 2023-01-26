0be724cc94b53e48152373ff51e58beb70192d67
FabioPinheiro <fabiomgpinheiro@gmail.com>
2023-01-26 10:26:10 +0000

    Add missing file from the last commit

--- did/shared/src/main/scala/fmgp/did/comm/package.scala
+++ did/shared/src/main/scala/fmgp/did/comm/package.scala
@@ -10,7 +10,6 @@ package object comm {
     def apply(fields: (String, Json)*): Json.Obj = Json.Obj(fields: _*)
   }
 
-  type Payload = String // Base64URL // TODO REMOVE
   type JWM_PROTECTED = String // TODO Base64 (URL)
   type JWM_SIGNATURE = String // TODO create a opaque for type safe
 
