e3cdfa539e7628654cbe7c7109d6768959a8bee3
Shailesh Patil <shailesh.patil@iohk.io>
2023-05-31 16:22:25 +0100

    WIP

---
+++ did-mediator/src/main/scala/fmgp/did/db/ReactiveMongoRepo.scala
@@ -0,0 +1,40 @@
+package fmgp.did.db
+
+import reactivemongo.api.bson._
+import reactivemongo.api.bson.collection.BSONCollection
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.api.Cursor
+import zio.Task
+import scala.concurrent.ExecutionContext
+import zio.ZIO
+import reactivemongo.api.CursorProducer
+
+abstract class ReactiveMongoRepo(
+    reactiveMongoApi: ReactiveMongoApi,
+    collectionName: String,
+)(using ec: ExecutionContext) {
+  def collection: Task[BSONCollection] = reactiveMongoApi.database
+    .map(_.collection(collectionName))
+
+  def insertOne[T](value: T)(using
+      w: BSONDocumentWriter[T]
+  ): Task[WriteResult] = {
+    for {
+      coll <- collection
+      result <- ZIO.fromFuture(implicit ec => coll.insert.one(value))
+    } yield result
+  }
+
+  def findOne[T](selector: BSONDocument, projection: Option[BSONDocument])(using
+      r: BSONDocumentReader[T],
+      p: CursorProducer[T]
+  ): Task[T] = {
+    for {
+      coll <- collection
+      result <- ZIO.fromFuture(
+        coll.find(selector, projection).cursor[T]().collect[List](-1, Cursor.FailOnError[List[T]]())
+      )
+    } yield result
+  }
+
+}
