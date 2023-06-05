e6da9a2cabdf91e0ef71023bbd7ae9e81f616e7a
Shailesh Patil <shailesh.patil@iohk.io>
2023-06-05 11:55:01 +0100

    added the collection in init script

--- initdb.js
+++ initdb.js
@@ -4,4 +4,17 @@ db.createUser({
     roles: [
         { role: "readWrite", db: "mediator" }
     ]
-})
+});
+
+const database = 'mediator';
+const collectionDidAccount = 'did_account';
+const collectionMessages = 'messages';
+
+// The current database to use.
+use(database);
+
+// Create  collections.
+db.createCollection(collectionDidAccount);
+db.createCollection(collectionMessages);
+//create index
+db.collectionDidAccount.createIndex({ 'alias': 1 }, { unique: true });
