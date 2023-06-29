576d7a3090598eca325b7c5ddc9834298253ba8b
Shailesh Patil <53746241+mineme0110@users.noreply.github.com>
2023-06-29 14:47:12 +0100

    fix: mediator unique constraint issue (#25)

--- initdb.js
+++ initdb.js
@@ -17,5 +17,6 @@ db.createCollection(collectionDidAccount);
 db.createCollection(collectionMessages);
 //create index
 db.getCollection(collectionDidAccount).createIndex({ 'did': 1 }, { unique: true });
-db.getCollection(collectionDidAccount).createIndex({ 'alias': 1 }, { unique: true });
+// Only enforce uniqueness on non-empty arrays
+db.getCollection(collectionDidAccount).createIndex({ 'alias': 1 }, { unique: true , partialFilterExpression: { "alias.0": { $exists: true } }});
 db.getCollection(collectionDidAccount).createIndex({ "messagesRef.hash": 1, "messagesRef.recipient": 1 });
