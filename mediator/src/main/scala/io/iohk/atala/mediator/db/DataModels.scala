cfa36e94a1de81ecb082ebf59dff57aa83dfb7dd
Fabio Pinheiro <fabiomgpinheiro@gmail.com>
2023-08-21 17:11:25 +0100

    feat: Store outbound messages (#84)
    Store all outbound messages
    Minor bug fix also:
    - The reply must only be in one way
    
    Signed-off-by: Fabio Pinheiro <fabiomgpinheiro@gmail.com>
    Signed-off-by: mineme0110 <shailesh.patil@iohk.io>

--- mediator/src/main/scala/io/iohk/atala/mediator/db/DataModels.scala
+++ mediator/src/main/scala/io/iohk/atala/mediator/db/DataModels.scala
@@ -3,8 +3,9 @@ package io.iohk.atala.mediator.db
 import fmgp.did.*
 import fmgp.did.comm.*
 import reactivemongo.api.bson.*
-
 import java.time.Instant
+import scala.util.Try
+
 type HASH = String
 // messages
 
@@ -37,4 +38,72 @@ case class DidAccount(
 object DidAccount {
   given BSONDocumentWriter[DidAccount] = Macros.writer[DidAccount]
   given BSONDocumentReader[DidAccount] = Macros.reader[DidAccount]
+}
+
+// messages outbox
+case class SentMessageItem(
+    _id: BSONObjectID = BSONObjectID.generate(),
+    encrypt: EncryptedMessage,
+    hash: HASH,
+    headers: ProtectedHeader,
+    plaintext: PlaintextMessage,
+    transport: Seq[SentMessageItem.TransportInfo],
+)
+
+object SentMessageItem {
+
+  def apply(
+      msg: EncryptedMessage,
+      plaintext: PlaintextMessage,
+      recipient: Set[TO],
+      distination: Option[String],
+      sendMethod: MessageSendMethod,
+      result: Option[String]
+  ): SentMessageItem = {
+    new SentMessageItem(
+      encrypt = msg,
+      hash = msg.sha1,
+      headers = msg.`protected`.obj,
+      plaintext = plaintext,
+      transport = Seq(
+        TransportInfo(recipient = recipient, distination = distination, sendMethod = sendMethod, result = result)
+      )
+    )
+  }
+
+  given BSONDocumentWriter[SentMessageItem] = {
+    import SentMessageItem.given_BSONDocumentWriter_TransportInfo
+    Macros.writer[SentMessageItem]
+  }
+  given BSONDocumentReader[SentMessageItem] = {
+    import SentMessageItem.given_BSONDocumentReader_TransportInfo
+    Macros.reader[SentMessageItem]
+  }
+
+  case class TransportInfo(
+      recipient: Set[TO],
+      distination: Option[String],
+      sendMethod: MessageSendMethod,
+      timestamp: BSONDateTime = BSONDateTime(Instant.now().toEpochMilli()), // Long,
+      result: Option[String],
+  )
+  object SentMessageItem {
+    given BSONDocumentWriter[TransportInfo] = Macros.writer[TransportInfo]
+    given BSONDocumentReader[TransportInfo] = Macros.reader[TransportInfo]
+  }
+}
+
+enum MessageSendMethod {
+  case HTTPS_POST extends MessageSendMethod
+  case INLINE_REPLY extends MessageSendMethod
+}
+object MessageSendMethod {
+  given BSONWriter[MessageSendMethod] with {
+    def writeTry(obj: MessageSendMethod): Try[BSONValue] =
+      Try(BSONString(obj.toString))
+  }
+  given BSONReader[MessageSendMethod] with {
+    def readTry(bson: BSONValue): Try[MessageSendMethod] =
+      bson.asTry[String].flatMap(v => Try(MessageSendMethod.valueOf(v)))
+  }
 }
